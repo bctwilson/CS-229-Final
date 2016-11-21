9ad51d4b1c2ecd6e5add0898903550095c3f2cf4
Blane Wilson <bctwilson@stanford.edu>
2016-11-21 03:38:40 -0700

    Finally dealt with updating dictionary with povknownratio. Now need to move to the rest of the variables. How will I deal with traffic score?

--- process_state.py
+++ process_state.py
@@ -48,65 +48,99 @@ fieldsWanted = [
 	"REGION", 
 	"area", 
 	"pop", 
-
 	"lowinc", 
 	"povknownratio", 
-
 	"mins", 
-
 	"under5", 
-
 	"over64", 
-
 	"age25up", 
 	"lths", 
-
 	"hhlds", 
 	"lingiso", 
-
 	"builtunits", 
 	"pre1960", 
 	"traffic.score"
 	]
 
+fieldsToUpdate = [
+	"area", 
+	"pop", 
+	"lowinc", 
+	"povknownratio", 
+	"mins", 
+	"under5", 
+	"over64", 
+	"age25up", 
+	"lths", 
+	"hhlds", 
+	"lingiso", 
+	"builtunits", 
+	"pre1960", 
+	"traffic.score"
+]
 
 
-
-global tracts_seen
 
 numCensusTracts = 0
 prev_tract = ""
 rawEntriesToWrite_Dict = {} # These entries will have data lines from each census TRACT
 rawEntriesToWrite = [] # These entries will have data lines from each census TRACT
 
-def createNewEntry(row, tract):
-	rawEntriesToWrite_Dict[tract] = ["dummy"]
+def calculate_unknown_pov(row, longHeader):
+	return int(row[longHeader.index("pop")]) - int(row[longHeader.index("povknownratio")])
 
-def updateTract(row):
-	d = 13
+
+def createNewEntry(row, tract, longHeader):
+	newRawEntry = []
+	for index in fieldIndex: # Write desired data into a python list. 
+		newRawEntry.append(row[index]) 
+	newRawEntry[1] = tract # Exclude last digit!!!
+	newRawEntry[fieldsWanted.index("povknownratio")] =  calculate_unknown_pov(row, longHeader) # Actually want to keep track of number of unknown pov status
+	print "New entry!: "
+	print newRawEntry
+	rawEntriesToWrite_Dict[tract] = newRawEntry
+
+def updateTract(row, tract, longHeader):
+	# Build a new entry. Take the values of the old entry, and add to the values, based on what was discussed. Then update dictionary key with
+	# new entry 
+	newEntry = rawEntriesToWrite_Dict[tract] # Copy old entry to this, to be updated, and then replace in dictionary
+	for field in fieldsToUpdate:
+		if (field == "povknownratio"): # You're actually updating # of unknown poverty status in your tract
+		  curr_unknownPov = int(newEntry[fieldsWanted.index("povknownratio")])
+		  newEntry[fieldsWanted.index("povknownratio")] = str(curr_unknownPov+calculate_unknown_pov(row, longHeader))
+  	
+  	rawEntriesToWrite_Dict[tract] = newEntry
+  	print "Updated Tract: "
+  	print newEntry
+
 
 def getCensusTractFromList(row):
 	FIPS = row[1]
 	tract = FIPS[0:11] # Only take first 11 digits, exclude the block number
 	return tract
 
-def updateDictionary(row):
+def updateDictionary(row, longHeader):
 	tract = getCensusTractFromList(row)
 	if tract in rawEntriesToWrite_Dict.keys():
-		df = 131 # updateTract(row) 
+		updateTract(row, tract, longHeader) 
 	else:
-		createNewEntry(row, tract)
+		createNewEntry(row, tract, longHeader)
 
 
 
 # Get index of each field, from the first row
 def getFieldIndex(row, fieldIndex):
+	global longHeader
 	shortHeader = [];
+	longHeader = row; 
 	for field in fieldsWanted:
 		ind = row.index(field)
 		fieldIndex.append(ind)
 		shortHeader.append(row[ind])
+	shortHeader[8] = "povUnknown"
 	rawEntriesToWrite.append(shortHeader)
+	print shortHeader
+	return longHeader
 
 def addEntry(row):
 	updateDictionary(row) 
@@ -137,26 +171,28 @@ def addEntry(row):
 #     f.close()
 # print "Write complete!"
 
-numLinesToOpen = -1
+numLinesToOpen = 5
 iterNum = 0
 print "Reading File..."
 
 with open('EJScreen_SocioDemographic_California_FIPS_Sorted.csv', 'rU') as f: 
     reader = csv.reader(f)
     fieldIndex = []
-    curr_Tract_toWrite = [];
+    longHeader = [];
     numCensusTracts = 0;
     for row in reader:
     	if (iterNum == numLinesToOpen):
     		break
     	if (iterNum != 0):
-        	updateDictionary(row)     
+        	updateDictionary(row, longHeader)     
     	else:
-    		getFieldIndex(row, fieldIndex)
+    		longHeader = getFieldIndex(row, fieldIndex)
         iterNum +=1
 print "Fileread Complete! \n"
 
 print "Number of Census Tracts: %d " %(len(rawEntriesToWrite_Dict))
+
+print "Now it's time to display dictionary... "
 
 
 
