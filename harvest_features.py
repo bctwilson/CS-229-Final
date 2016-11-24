16852c11607d2bf9fdef1f67e7bc06ba6e6a15b6
Blane Wilson <bctwilson@stanford.edu>
2016-11-23 19:53:47 -0700

    Added lingusitically isolated households to percentile sheets for 50 states

--- harvest_features.py
+++ harvest_features.py
@@ -40,6 +40,21 @@ def getTractLTHS(row,fileHeader):
 def getTract25(row,fileHeader):
 	return float(row[fileHeader.index("age25up")])
 
+def getTract5(row, fileHeader):
+	return float(row[fileHeader.index("under5")])
+
+def getTracthhlds(row, fileHeader):
+	return float(row[fileHeader.index("hhlds")])
+
+def getTractlingiso(row, fileHeader):
+	return float(row[fileHeader.index("lingiso")])
+
+def lingiso_pct(row, fileHeader):
+	return 100*(getTractlingiso(row,fileHeader))/getTracthhlds(row,fileHeader)
+
+def children_pct(row, fileHeader):
+	return 100*(getTract5(row,fileHeader))/getTractPop(row,fileHeader)
+
 def popden(row, fileHeader):
 	return (getTractPop(row, fileHeader)/getTractArea(row,fileHeader))
 
@@ -50,18 +65,24 @@ def lths_pct(row, fileHeader):
 	return 100*(getTractLTHS(row, fileHeader)/getTract25(row,fileHeader))
 
 def add_pct_row(row, fileHeader):
-	if (getTractPop(row,fileHeader) != 0 and getTract25(row,fileHeader) != 0):
+	if (getTractPop(row,fileHeader) != 0 and getTract25(row,fileHeader) != 0 and getTracthhlds(row,fileHeader) != 0):
 		newList = []
 		popden_Val = popden(row, fileHeader) 
 		min_pctVal = min_pct(row, fileHeader)
 		lths_pctVal = lths_pct(row, fileHeader)
+		children_pctVal = children_pct(row, fileHeader)
+		lingiso_pctVal = lingiso_pct(row, fileHeader)
 		newList.append(popden_Val)
 		newList.append(min_pctVal) 
-		newList.append(lths_pctVal) 
+		newList.append(lths_pctVal)
+		newList.append(children_pctVal) 
+		newList.append(lingiso_pctVal)
 		percDict[getTractFIPS(row,fileHeader)] = newList
 		popDens_values.append(popden_Val)
 		mins_values.append(min_pctVal)
 		lths_values.append(lths_pctVal)
+		children_values.append(children_pctVal)
+		lingiso_values.append(lingiso_pctVal)
 	else: 
 		no_pop_or_no_over_25.append(getTractFIPS(row,fileHeader))
 
@@ -86,6 +107,8 @@ for filename in os.listdir(directory):
 	popDens_values = []
 	mins_values = []
 	lths_values = []
+	children_values = []
+	lingiso_values = []
 	no_pop_or_no_over_25 = []
 	print "Reading File..."
 	with open(directory + "/"+ "EJScreen_SocioDemographic_FIPS_TRACTS_Sorted_" + curr_state + ".csv", 'rU') as f: 
@@ -107,11 +130,13 @@ for filename in os.listdir(directory):
 	feature_list.append(np.array(popDens_values))
 	feature_list.append(np.array(mins_values))
 	feature_list.append(np.array(lths_values))
+	feature_list.append(np.array(children_values))
+	feature_list.append(np.array(lingiso_values))
 
 
 	outfile_print = []
-	perc_header = ["popdense", "percmin", "perclths"]
-	outfile_header = ["FIPS", "popdense", "pctpopdense", "percmin", "pctmin", "perclths", "pctlths"]
+	perc_header = ["popdense", "percmin", "perclths", "percchildren", "perclingiso"]
+	outfile_header = ["FIPS", "popdense", "pctpopdense", "percmin", "pctmin", "perclths", "pctlths", "percchildren", "pctchildren", "perclingisohlds", "pctlingisohlds"]
 	for FIPS in percDict:
 		curr_percentages = percDict[FIPS] 
 		outfile_line = [FIPS]
@@ -125,16 +150,15 @@ for filename in os.listdir(directory):
 	outfile_print_Sorted = sorted(outfile_print[1:len(outfile_print)], key=lambda entry: int(entry[0]))
 	outfile_print_Sorted.insert(0,outfile_header)
 
+	print "Writing " + curr_state + " Percentiles for Best Features to CSV File..."
+	outFileName = "CalEnviroScreen_tract_Children_Lingisohhlds_FeaturePercentiles_" + curr_state + ".csv"
+	with open(outFileName, "wb") as f:
+	    writer = csv.writer(f)
+	    writer.writerows(outfile_print_Sorted)
+	    f.close()
+	print "Write complete! \n"
 
-	# print "Writing " + curr_state + " Percentiles for Best Features to CSV File..."
-	# outFileName = "CalEnviroScreen_tract_FeaturePercentiles_" + curr_state + ".csv"
-	# with open(outFileName, "wb") as f:
-	#     writer = csv.writer(f)
-	#     writer.writerows(outfile_print_Sorted)
-	#     f.close()
-	# print "Write complete! \n"
-
-	nopeStr =  "The following Census Tracts either had 0 population or 0 people over 25 years old: "
+	nopeStr =  "The following Census Tracts either had 0 population, 0 people over 25 years old, or 0 households: "
 	no_pop_or_no_over_25.insert(0,nopeStr)
 	for tract in no_pop_or_no_over_25:
 		print tract
